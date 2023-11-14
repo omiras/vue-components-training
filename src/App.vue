6879d74625821e8502ead81ee84a2e86d03bcfbe
omiras <oscar.miras@gmail.com>
2023-11-14 13:35:37 +0100

    idCard

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@ import IdCard from './components/IdCard.vue';
     <h2>Id Card</h2>
     <IdCard last-name='Doe' first-name='John' gender='male' :height="178" :birth='new Date("1992-07-14")'
       picture="https://randomuser.me/api/portraits/men/44.jpg" />
+    <IdCard last-name='Delores ' first-name='Obrien' gender='female' :height="172" :birth='new Date("1988-05-11")'
+      picture="https://randomuser.me/api/portraits/women/44.jpg" />
+
   </section>
 </template>
 
--- src/components/IdCard.vue
+++ src/components/IdCard.vue
@@ -1,11 +1,14 @@
 <script setup>
-defineProps(['lastName', 'firstName'])
+defineProps(['lastName', 'firstName', 'gender', 'height', 'birth'])
 </script>
 
 <template>
     <div>
         <p><strong>First Name:</strong> {{ firstName }}</p>
         <p><strong>Last Name:</strong> {{ lastName }}</p>
+        <p><strong>Gender:</strong> {{ gender }}</p>
+        <p><strong>Height:</strong>{{ height / 100 }}m</p>
+        <p><strong>Birth:</strong> {{ birth.toDateString() }}</p>
 
     </div>
 </template>
