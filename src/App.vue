12fa9f5825fd6145def8b34d9a8969f1a09ae664
omiras <oscar.miras@gmail.com>
2025-08-10 15:37:40 +0200

    done iteration 1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-
+import IdCard from './components/IdCard.vue';
 </script>
 
 <template>
   <h1>Creación de componentes con Vue</h1>
   <p>Crea a conitnuación todos los componentes del ejercicio.</p>
+  <h2>Id Cards</h2>
+  <IdCard picture="https://randomuser.me/api/portraits/men/44.jpg" firstName="John" lastName="Tremor" gender="male" :height="178" :birth="new Date('1985-02-11')"/>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/IdCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps(['firstName', 'lastName', 'gender', 'height', 'birth', 'picture'])
+</script>
+
+<template>
+    <div class="card">
+
+        <img :src="picture" alt="face photo">
+        <div>
+            <p><strong>First Name: </strong> {{ firstName }}</p>
+            <p><strong>Last Name: </strong>{{ lastName }}</p>
+            <p><strong>Gender: </strong>{{ gender }}</p>
+            <p><strong>Height: </strong> {{ height / 100 }}m</p>
+            <p><strong>Birth: </strong> {{ birth.toLocaleDateString()  }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    display: flex;
+    gap: 1rem;
+    border: 1px solid #ccc;
+    padding: 5px;
+
+}
+
+</style>
