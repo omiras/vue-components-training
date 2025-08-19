dd26b431e051df8d0856c1bf9d4a2f0bf1489d7d
omiras <oscar.miras@gmail.com>
2025-08-19 19:18:44 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,21 @@ import Rating from './components/Rating.vue';
   <h1>Creación de componentes con Vue</h1>
   <p>Crea a conitnuación todos los componentes del ejercicio.</p>
   <h2>Id Cards</h2>
-  <IdCard picture="https://randomuser.me/api/portraits/men/44.jpg" firstName="John" lastName="Tremor" gender="male" :height="178" :birth="new Date('1985-02-11')"/>
+  <IdCard picture="https://randomuser.me/api/portraits/men/44.jpg" firstName="John" lastName="Tremor" gender="male"
+    :height="178" :birth="new Date('1985-02-11')" />
   <h2>Random</h2>
   <Random :min="1" :max="10"></Random>
   <h3>RGB Color</h3>
   <BoxColor :r="255" :g="0" :b="0" />
   <h3>Credit Card</h3>
-  <CreditCard type="visa" bg-color="#11aa99" number="0123456789018845" :expiration-month="3" :expiration-year="2021" bank="N26" owner="Maxenne Bourete"/>
+  <CreditCard type="visa" bg-color="#11aa99" number="0123456789018845" :expiration-month="3" :expiration-year="2021"
+    bank="N26" owner="Maxenne Bourete" />
   <h3>Rating</h3>
+  <Rating :rate="0" />
+  <Rating :rate="1.49" />
+  <Rating :rate="1.5" />
+  <Rating :rate="3" />
+  <Rating :rate="4" />
   <Rating :rate="5" />
 </template>
 
