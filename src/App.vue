aafbea745b87fc93430c87de0c20f11759f6ef3e
omiras <oscar.miras@gmail.com>
2025-08-19 19:00:08 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
+<!-- single file components: SFC. -->
+
+<!-- Lógica JavaScript y reactividad -->
 <script setup>
+import { ref } from 'vue'; 
+import IdCard from './components/IdCard.vue';
+import catPicture from './assets/images/gato.png'
+
+const mariaHeight = ref(199)
 
 </script>
 
+<!-- HTML y directivas Vue -->
 <template>
-  <h1>Creación de componentes con Vue</h1>
-  <p>Crea a continuación todos los componentes del ejercicio.</p>
+  <div>
+    <h1>Creación de componentes con Vue</h1>
+    <p>Crea a continuación todos los componentes del ejercicio.</p>
+    <h2>1-Id Card</h2>
+    <IdCard first-name="Maria" last-name="Dolores" gender="female" :height="mariaHeight"
+      picture="https://randomuser.me/api/portraits/men/44.jpg" :birth="new Date('1999-05-11')" />
+      <!-- Es interesante pasar un objeto de tipo fecha en vez de un string tipo '1988-05-11' porque de este tipo de objeto puedes extraer un formato de fecha, puedes comparar fechas, etc -->
+    <IdCard last-name='Delores' first-name='Obrien' gender='female' :height="172" :birth="new Date('1988-05-11')"
+      picture="https://randomuser.me/api/portraits/women/44.jpg" />
+   <IdCard last-name='Garcia' first-name='Loki' gender='male' :height="172" :birth="new Date('1988-05-11')"
+      :picture="catPicture" />
+  
+  </div>
 </template>
 
+<!-- Estilos. Con el modificador "scoped" permite que estos estilos solo se apliquen a este componente -->
 <style scoped>
-
+h2 {
+  margin: 1rem 0;
+}
 </style>
---
+++ src/components/IdCard.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+// La variable firstName que es informada por el componente padre esta disponible indemediatamente en el <template>
+defineProps({
+    lastName: String,
+    firstName: String,
+    height: Number,
+    gender: String, //male, female, non-binary
+    picture: String,
+    birth: Date
+})
+
+// Aquí va todo el JavaScvript
+</script>
+
+<template>
+ <!-- Recomiendo empezar haciendo una pequeña parte estática-->
+  <!-- Cuando veas que mas o menos se empieza a ver como quieres, empezar a pasar propiedades -->
+  <div class="container">
+    <img :src="picture" alt="">
+    <div>
+        <p><strong>First name: </strong>{{ firstName.toUpperCase() }}</p>
+        <p><strong>Last name: </strong>{{ lastName }}</p>
+        <p><strong>Height: </strong> {{ height / 100 }}m</p>
+        <p><strong>Gender: </strong> {{ gender }}</p>
+        <p><strong>Birth:</strong> {{ birth.toDateString() }}</p>
+    </div>
+  </div>
+</template>
+
+<!-- El modificar scoped permite que todos los estilos definidos aquí se apliquen solamente al componente -->
+<style scoped>
+
+div > img {
+    margin-right: 1rem;
+    width: 100px;
+    
+}
+
+div.container {
+    display: flex;
+    border: 2px solid #ccc;
+    padding: 1rem;
+}
+</style>
+
