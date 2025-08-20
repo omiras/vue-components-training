05de987016b0a8925f467a03e3bb7d46338f57db
omiras <oscar.miras@gmail.com>
2025-08-20 19:32:09 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 
 <!-- Lógica JavaScript y reactividad -->
 <script setup>
-import { ref } from 'vue'; 
+import { ref } from 'vue';
 import IdCard from './components/IdCard.vue';
 import BoxColor from './components/BoxColor.vue';
 
 import catPicture from './assets/images/gato.png'
+import LikeButton from './components/LikeButton.vue';
 
 const mariaHeight = ref(199)
 
@@ -17,19 +18,23 @@ const mariaHeight = ref(199)
   <div>
     <h1>Creación de componentes con Vue</h1>
     <p>Crea a continuación todos los componentes del ejercicio.</p>
+ <h2>Like Button</h2>
+    <LikeButton />
+
     <h2>1-Id Card</h2>
     <IdCard first-name="Maria" last-name="Dolores" gender="female" :height="mariaHeight"
       picture="https://randomuser.me/api/portraits/men/44.jpg" :birth="new Date('1999-05-11')" />
-      <!-- Es interesante pasar un objeto de tipo fecha en vez de un string tipo '1988-05-11' porque de este tipo de objeto puedes extraer un formato de fecha, puedes comparar fechas, etc -->
+    <!-- Es interesante pasar un objeto de tipo fecha en vez de un string tipo '1988-05-11' porque de este tipo de objeto puedes extraer un formato de fecha, puedes comparar fechas, etc -->
     <IdCard last-name='Delores' first-name='Obrien' gender='female' :height="172" :birth="new Date('1988-05-11')"
       picture="https://randomuser.me/api/portraits/women/44.jpg" />
-   <IdCard last-name='Garcia' first-name='Loki' gender='male' :height="172" :birth="new Date('1988-05-11')"
+    <IdCard last-name='Garcia' first-name='Loki' gender='male' :height="172" :birth="new Date('1988-05-11')"
       :picture="catPicture" />
-   <h2>Box Color</h2>
-   <BoxColor :r="255" :g="0" :b="0" />
-      <BoxColor :r="0" :g="255" :b="0" />
 
+    <h2>Box Color</h2>
+    <BoxColor :r="255" :g="0" :b="0" />
+    <BoxColor :r="0" :g="255" :b="0" />
 
+   
   </div>
 </template>
 
---
+++ src/components/LikeButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed, ref } from 'vue';
+
+const colors = ['red', 'green', 'blue', '#125467', 'rgb(233,111,44)']
+
+// 1. Primero ver si tu variable de estado se comporta como esperas cambiando su valor inicial 
+const likes = ref(0)
+
+const incrementLikes = () => likes.value++
+
+const buttonColor = computed(()=> {
+    return colors[likes.value % colors.length]
+})
+
+</script>
+
+<template>
+<button :style="{backgroundColor: buttonColor}" @click="incrementLikes">{{ likes }} Likes</button>
+</template>
+
+<style scoped>
+button {
+    cursor: pointer;
+}
+</style>
